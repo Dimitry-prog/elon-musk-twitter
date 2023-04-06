6c3f86ed8742f3296417b9312965ee9c78ff55e2
Dimitry-prog <kfifa3@gmail.com>
2023-04-06 22:48:08 +0300

    feat: Button

---
+++ app/components/UI/Button.tsx
@@ -0,0 +1,35 @@
+import {FC} from 'react';
+
+type ButtonProps = {
+  label: string;
+  onClick?: () => void;
+  secondary?: boolean;
+  fullWidth?: boolean;
+  large?: boolean;
+  outline?: boolean;
+  disabled?: boolean;
+};
+
+const Button: FC<ButtonProps> = ({
+                                   label,
+                                   onClick,
+                                   secondary,
+                                   fullWidth,
+                                   large,
+                                   outline,
+                                   disabled
+                                 }) => {
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      aria-label={label}
+      disabled={disabled}
+      className={`font-semibold rounded-full border-2 hover:opacity-80 disabled:opacity-60 disabled:cursor-not-allowed transition duration-500 ${fullWidth ? 'w-full' : 'w-fit'} ${secondary ? 'dark:bg-slate-50/50 border-slate-800' : 'bg-sky-500 dark:text-white border-sky-500'} ${large ? 'px-5 py-3 text-xl' : 'px-4 py-2 text-base'} ${outline ? 'dark:text-white dark:border-white bg-transparent' : ''}`}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Button from "@/app/components/UI/Button";
+
 export default function Home() {
+  const handleClick = () => {
+
+  }
   return (
-    <main className="p-4 flex flex-col gap-4 flex-grow">
+    <main className="p-4 flex flex-col gap-4 flex-grow border-x border-slate-500/10 dark:border-slate-800">
       <h1 className="text-4xl font-bold dark:text-white/90">Hello Home!</h1>
+      <Button label="Hello"/>
     </main>
   )
 }
